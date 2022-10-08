5868674584d1da4f810893f34a59de75ef0af1dc
carmelariadas <carmen.mk.167@gmail.com>
2022-10-08 20:57:17 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "👑", "👸", "A"];
+  let palos = ["♦", "♥", "♠", "♣"];
+
+  const getARandomFromAnyArray = array => {
+    const positionRandomInArray = Math.floor(Math.random() * array.length);
+    const valueRandomInArray = array[positionRandomInArray];
+    return valueRandomInArray;
+  };
+
+  const h1Numbers = document.querySelector("#numberOfCard");
+  const h1Palos = document.querySelector(".paloOfCard");
+  const h1Palos2 = document.querySelector(".paloOfCard2");
+
+  h1Numbers.innerHTML = getARandomFromAnyArray(numbers);
+  h1Palos.innerHTML = getARandomFromAnyArray(palos);
+  h1Palos2.innerHTML = h1Palos.innerHTML;
 };
